e60e9a0e515514f831da0a295cb0879972bdb30b
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-08-31 13:51:50 +0300

    commit tree 7a6db9e54a6d099731f99c34b54cae2ab8874e8e

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors , through: :characters
+    belongs_to :network
   
-end
+    def actors_list 
+    def actors_list le
+      self.actors.map do |actor|
+          "#{actor.first_name} #{actor.last_name}"
+      end
+  end
+  end
